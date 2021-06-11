84778d3c1e019bd25d726cd52e8e709aa8491018
Igor Degtiarov <igoryok.d@gmail.com>
2021-06-11 11:27:45 +0300

    Revert "[BD-26] Check if exam can be continued whe attempt status is ready_to_submit (#35)"
    This reverts commit b116c88063296e8846ee9ec97f9e60ac901cdb2d.

--- edx_proctoring/api.py
+++ edx_proctoring/api.py
@@ -630,6 +630,7 @@ def get_exam_attempt_data(exam_id, attempt_id, is_learning_mfe=False):
     # a same process as the LMS
     if is_learning_mfe:
         exam_url_path = resolve_exam_url_for_learning_mfe(exam['course_id'], exam['content_id'])
+
     else:
         exam_url_path = reverse('jump_to', args=[exam['course_id'], exam['content_id']])
 
@@ -654,11 +655,6 @@ def get_exam_attempt_data(exam_id, attempt_id, is_learning_mfe=False):
             args=[attempt['id']]
         ),
     }
-
-    if attempt['status'] == ProctoredExamStudentAttemptStatus.ready_to_submit:
-        if (can_continue := _does_time_remain(attempt)) and exam['is_proctored']:
-            can_continue = provider and not provider.should_block_access_to_exam_material()
-        attempt_data['can_continue'] = can_continue
 
     if provider:
         attempt_data.update({
