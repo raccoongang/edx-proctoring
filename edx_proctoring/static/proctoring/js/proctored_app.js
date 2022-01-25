63a815a5a3253dc4283a9a80dc079ac20c741193
Peter Kulko RG <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2022-01-25 11:17:06 +0200

    feat: [EXAM-1844] added jQuery logics for camera checker (#57)
    * feat: [EXAM-1844] added jQuery logics for camera checker
    
    * style: [EXAM-1844] refactoring jQuery scripts
    
    * fix: [EXAM-1844] refactoring translations
    
    * chore: [EXAM-1844] Updates package version to 2.7.0
    
    Co-authored-by: NikolayBorovenskiy <nikolay.borovenskiy@gmail.com>

--- edx_proctoring/static/proctoring/js/proctored_app.js
+++ edx_proctoring/static/proctoring/js/proctored_app.js
@@ -8,4 +8,18 @@ $(function() {
         model: new ProctoredExamModel()
     });
     proctoredExamView.render();
+
+    $('.check-camera-video-start').on('click', function () {
+        $(this).attr('disabled', true);
+        const $blockVideo = $('#check-camera-video-block', this.element)[0];
+        try {
+            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices.getUserMedia({video: true}).then(function (stream) {
+                    $blockVideo.srcObject = stream;
+                });
+            }
+        } catch (e) {
+            console.error('The Video checker failed, the following error occurred:', e);
+        }
+    });
 });
