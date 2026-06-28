d32ef61b15351f1fc92da1b6f2dd61ccf102c1dc
Yurii Borovenskyi <yurii.borovenskyi@raccoongang.com>
2026-06-28 21:40:42 +0300

    feat: [EXAM-2964] add routing key for proctored exam attempt removal

--- edx_proctoring/settings/common.py
+++ edx_proctoring/settings/common.py
@@ -7,6 +7,7 @@ def plugin_settings(settings):
     """
     Injects local settings into django settings
     """
+    settings.PROCTORED_EXAM_ATTEMPT_REMOVAL_ROUTING_KEY = getattr(settings, 'HIGH_MEM_QUEUE', None),
     settings.PROCTORING_SETTINGS = {}
     settings.PROCTORING_BACKENDS = {
         'DEFAULT': 'null',
--- edx_proctoring/settings/production.py
+++ edx_proctoring/settings/production.py
@@ -5,6 +5,14 @@ def plugin_settings(settings):
     "Injects local settings into django settings"
     auth_tokens = getattr(settings, 'AUTH_TOKENS', {})
     env_tokens = getattr(settings, 'ENV_TOKENS', {})
+    settings.PROCTORED_EXAM_ATTEMPT_REMOVAL_ROUTING_KEY = env_tokens.get(
+        'PROCTORED_EXAM_ATTEMPT_REMOVAL_ROUTING_KEY',
+        getattr(
+            settings,
+            'PROCTORED_EXAM_ATTEMPT_REMOVAL_ROUTING_KEY',
+            getattr(settings, 'HIGH_MEM_QUEUE', None),
+        ),
+    )
     if env_tokens.get('PROCTORING_SETTINGS'):
         settings.PROCTORING_SETTINGS = env_tokens['PROCTORING_SETTINGS']
     if auth_tokens.get('PROCTORING_BACKENDS'):
